089c2e9d684487a5a0efa88f9b37140102d6a91a
Dansprouls <dspr1015@gmail.com>
2023-06-06 12:23:11 -0400

    removed unnecessary comments

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -67,25 +67,3 @@ export const LoginView = ({ onLoggedIn }) => {
     </Form>
   );
 };
-
-/* return(
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input 
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        required />
-      </label>
-      <label>
-        Password:
-        <input 
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)} 
-        required/>
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  ); */
--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -80,47 +80,4 @@ export const SignupView = () => {
       </Button>
     </Form>
   );
-
-  /* return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input 
-          type="text"
-          value={username}
-          onChange= {(e) => setUsername(e.target.value)} 
-          required
-          minLength='3'
-        />
-      </label>
-      <label>
-        Password:
-        <input 
-          type="password" 
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Email:
-        <input 
-          type="email" 
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Birthday:
-        <input 
-          type="date"
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  ); */
 };
